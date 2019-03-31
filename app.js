476927a4bf8a6e0ed4500a58f8ad10e1b8e578db
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-03-31 13:39:12 -0600

    updating extra routes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var promtionRouter = require('./routes/promotionRouter');
+
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promtionRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
