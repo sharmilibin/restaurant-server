5ab259161431dea8465767e5be9e4e29f63e034e
Sharmi Libin <sharmi.m.rose@gmail.com>
2019-04-22 17:22:41 -0600

    population

--- models/dishes.js
+++ models/dishes.js
@@ -1,65 +1,66 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-require ('mongoose-currency').loadType(mongoose);
-const Currency=mongoose.Types.Currency;
-
-const commentSchema = new Schema({
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+var commentSchema = new Schema(
+  {
     rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
     },
     comment: {
-        type: String,
-        required: true
+      type: String,
+      required: true
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     }
-}, {
-        timestamps: true
-
-    }
-
+  },
+  {
+    timestamps: true
+  }
 );
-
-const dishSchema = new Schema({
+const dishSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     image: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     label: {
-        type: String,
-        default: ''
+      type: String,
+      default: ''
     },
     price: {
-        type: Currency,
-        required: true,
-        min: 0
+      type: Currency,
+      required: true,
+      min: 0
     },
     featured: {
-        type: Boolean,
-        default:false      
+      type: Boolean,
+      default: false
     },
-    comments:[commentSchema]
-}, {
+    comments: [commentSchema]
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 var Dishes = mongoose.model('Dish', dishSchema);
 
--- models/users.js
+++ models/users.js
@@ -2,13 +2,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-
 var User = new Schema({
-    
-    admin:   {
-        type: Boolean,
-        default: false
-    }
+  firstname: {
+    type: String,
+    default: ''
+  },
+  lastname: {
+    type: String,
+    default: ''
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  }
 });
 User.plugin(passportLocalMongoose);
 
